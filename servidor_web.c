e5d19b24ebb6ec43134e94e5287841a405b49f7b
Vivi <vlaurido@espol.edu.ec>
2017-09-02 10:47:34 -0500

    cambios servidor web

---
+++ servidor_web.c
@@ -0,0 +1,55 @@
+//TAL VEZ NO UTILICEMOS TODAS LAS LIBRERIAS
+//LAS SAQUE DEL ARCHIVO SERVIDOR.C
+#include <sys/types.h>          /* some systems still require this */
+#include <sys/stat.h>
+#include <stdio.h>              /* for convenience */
+#include <stdlib.h>             /* for convenience */
+#include <stddef.h>             /* for offsetof */
+#include <string.h>             /* for convenience */
+#include <unistd.h>             /* for convenience */
+#include <signal.h>             /* for SIG_ERR */ 
+#include <netdb.h> 
+#include <errno.h> 
+#include <syslog.h> 
+#include <sys/socket.h> 
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/resource.h>
+
+//Librerias http
+#include <sys/select.h>
+#include <microhttpd.h>
+
+//Función que es llamada cada vez que llega un request apropiado
+int answer_to_connection (void *cls, struct MHD_Connection *connection,
+                          const char *url,
+                          const char *method, const char *version,
+                          const char *upload_data,
+                          size_t *upload_data_size, void **con_cls)
+{
+    //Reply-String
+    const char *page  = "<html><body>Hello, browser!</body></html>";
+    struct MHD_Response *response;
+    int ret;
+  
+    response = MHD_create_response_from_buffer (strlen (page), (void*) page, MHD_RESPMEM_PERSISTENT);
+    ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
+    MHD_destroy_response (response);
+
+    return ret;
+}
+
+/*MAIN*/
+int main ()
+{
+  struct MHD_Daemon *daemon;
+
+  daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL,
+                             &answer_to_connection, NULL, MHD_OPTION_END);
+  if (NULL == daemon) return 1;
+  getchar ();
+
+  MHD_stop_daemon (daemon);
+  return 0;
+}
